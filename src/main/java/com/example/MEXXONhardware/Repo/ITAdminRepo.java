f375ef7667035724de829823e396f8aae288afca
mert <mert.inal@gmail.com>
2024-11-19 20:57:14 +0100

    first repo,

--- src/main/java/com/example/MEXXONhardware/Repo/ITAdminRepo.java
+++ src/main/java/com/example/MEXXONhardware/Repo/ITAdminRepo.java
@@ -1,2 +1,10 @@
-package com.example.MEXXONhardware.Repo;public interface ITAdminRepo {
+package com.example.MEXXONhardware.Repo;
+
+import com.example.MEXXONhardware.DB.ITAdmin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ITAdminRepo extends JpaRepository<ITAdmin, Long> {
+
 }
